564a072cf0cca6018a892229e8f7a53404ba08ba
Amanda Pereira <mandica_15@hotmail.com>
2016-04-13 09:11:04 -0500

    jfgbhncv

---
+++ infinity/Assets/ChangeSceneJapanese.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeSceneJapanese : MonoBehaviour{
+
+	void OnTriggerEnter(Collider co)
+	{
+		if (co.name == "Player")
+			UnityEngine.SceneManagement.SceneManager.LoadScene ("japanese house unity");
+	}
+}
---
+++ infinity/Assets/ChangeSceneStageOneD.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeSceneStageOneD : MonoBehaviour{
+
+	void OnTriggerEnter(Collider co)
+	{
+		if (co.name == "Player")
+			UnityEngine.SceneManagement.SceneManager.LoadScene ("first");
+	}
+}
